d67159cd3c8053d3c125675138360a9532879011
othelarian <le.maitre.killian@gmail.com>
2018-08-02 08:58:00 +0200

    scan test with internal led

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,132 @@
 
 #include "conf.h"
 
+// PLAN #########################################
+
+/*
+1. CONNECT TO SSIDs, with preferences --> IN PROGRESS
+2. RECEPTION OF UDP REQUEST
+3. SENDING UDP datagram with full informations
+4. CONNECT TO THE APP
+5. SETUP LED
+*/
+
+// GLOBAL VARIABLES #############################
+
+bool getnet = false;
+int sel_lvl = 0;
+String sel_ssid = "";
+String sel_pwd = "";
+
+// HELPERS ######################################
+
+bool scanning() {
+    bool res = false;
+    int nbn = WiFi.scanNetworks();
+    for (int i = 0; i < nbn; i++) {
+        String tmp_ssid = WiFi.SSID(i);
+        if (tmp_ssid == SSID_01) {
+            sel_ssid = SSID_01;
+            sel_pwd = PASS_01;
+            sel_lvl = 1;
+            return true;
+        }
+        else if (tmp_ssid == SSID_02) {
+            sel_ssid = SSID_02;
+            sel_pwd = PASS_02;
+            sel_lvl = 2;
+            res = true;
+        }
+    }
+    return res;
+}
+
+// GENERAL PART #################################
+
 void setup() {
-    //
+    // TEST PURPOSE
     pinMode(LED_BUILTIN, OUTPUT);
-    //
-    // TODO : scan for available SSIDs
-    // TODO : check each registered SSID
-    //
-    WiFi.begin(, );
-    //
-    //
+    // TEST END
+    // setup the wifi
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    // ready to go
+    delay(100);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    if (WiFi.status() == WL_CONNECTED) {
+    // if not connected, try to connect
+    if (!getnet) {
+        // TEST PURPOSE
         digitalWrite(LED_BUILTIN, HIGH);
+        // TEST END
+        getnet = scanning();
+        // TEST PURPOSE
+        digitalWrite(LED_BUILTIN, LOW);
         delay(500);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(200);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(400);
+        // TEST END
+        if (!getnet) {
+            // TEST PURPOSE
+            delay(300);
+            for (int i = 0; i < 3; i++) {
+                digitalWrite(LED_BUILTIN, HIGH);
+                delay(300);
+                digitalWrite(LED_BUILTIN, LOW);
+                delay(300);
+            }
+            // TEST END
+            // delay the next scan in 5 seconds
+            delay(5000);
+        }
+        else {
+            // connect to the selected network
+            WiFi.begin(sel_ssid.c_str(), sel_pwd.c_str());
+            //
+            // TODO : setup webserver ?
+            // TODO : setup udp ?
+            //
+        }
     }
     else {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
+        //
+        // TODO : check if we are connected. If not, disconnect and set "getnet" to false
+        //
+        switch(sel_lvl) {
+            case 1:
+                //
+                digitalWrite(LED_BUILTIN, HIGH);
+                delay(300);
+                digitalWrite(LED_BUILTIN, LOW);
+                delay(300);
+                digitalWrite(LED_BUILTIN, HIGH);
+                delay(700);
+                digitalWrite(LED_BUILTIN, LOW);
+                delay(300);
+                digitalWrite(LED_BUILTIN, HIGH);
+                delay(300);
+                digitalWrite(LED_BUILTIN, LOW);
+                delay(1200);
+                //
+                break;
+            case 2:
+                //
+                digitalWrite(LED_BUILTIN, HIGH);
+                delay(800);
+                digitalWrite(LED_BUILTIN, LOW);
+                delay(200);
+                digitalWrite(LED_BUILTIN, HIGH);
+                delay(800);
+                digitalWrite(LED_BUILTIN, LOW);
+                delay(1200);
+                //
+                break;
+            //
+        }
+        //
+        //
     }
+    //
+    /*
+    if (WiFi.status() == WL_CONNECTED) {
+    */
 }
